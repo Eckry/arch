389fa1fc79fb9ff9caebb741eaedbe0fbfe05be3
Eckry <112786854+Eckry@users.noreply.github.com>
2026-09-05 02:57:05 +0000

    hyprpaper

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+pacman_packages="$(cat "./packages/pacman.txt" | tr '\n' ' ')"
+aur_packages="$(cat "./packages/aur.txt" | tr '\n' ' ')"
+
+echo "====== INSTALLING PACMAN PACKAGES ====="
+sudo pacman -S --needed $pacman_packages
+
+echo "====== INSTALLING AUR ====="
+cd ..
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+cd ..
+cd arch
+
+echo "====== INSTALLING AUR PACKAGES ====="
+yay -S $aur_packages
+
+
