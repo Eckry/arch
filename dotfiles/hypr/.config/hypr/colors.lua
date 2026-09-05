398e59b0d6ceeae3fd81f9b2b5f257a84312a2d3
Eckry <112786854+Eckry@users.noreply.github.com>
2026-09-05 21:06:29 +0000

    add colors

---
+++ dotfiles/hypr/.config/hypr/colors.lua
@@ -0,0 +1,11 @@
+local colors = {
+	background = "#05292E",
+	background_alt = "#0C3935",
+	green_dark = "#235744",
+	green = "#47835D",
+	green_light = "#61A375",
+	accent = "#C8E690",
+	text = "#D4E994",
+}
+
+return colors
