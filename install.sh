5d5c3f73b50d99139750272190929d8e1551f055
Eckry <112786854+Eckry@users.noreply.github.com>
2026-09-05 19:05:27 -0600

    add ufw

--- install.sh
+++ install.sh
@@ -19,3 +19,11 @@ yay -S $aur_packages
 
 mkdir ~/Pictures
 cp ./wallpapers/wp.jpg ~/Pictures/wp.jpg
+
+sudo systemctl enable --now bluetooth
+sudo systemctl enable --now ntpd
+
+sudo ufw default deny incoming
+sudo ufw default allow outgoing
+
+sudo ufw enable
