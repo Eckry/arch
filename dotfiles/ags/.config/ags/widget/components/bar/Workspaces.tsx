5675c9eadccd2a86111a8de13561a7e5e2c61820
Eckry <112786854+Eckry@users.noreply.github.com>
2026-09-12 13:59:27 -0600

    add icons to workspaces

--- dotfiles/ags/.config/ags/widget/components/bar/Workspaces.tsx
+++ dotfiles/ags/.config/ags/widget/components/bar/Workspaces.tsx
@@ -1,14 +1,19 @@
 import { Gtk } from "ags/gtk4"
 import Hyprland from "gi://AstalHyprland"
 import { createBinding, For } from "gnim"
+import { iconMap } from "../../consts"
+
+function getIcon(className: string) {
+  return Object.hasOwn(iconMap, className) ? iconMap[className as keyof typeof iconMap] : iconMap.default
+}
 
 export default function Workspaces() {
   const hypr = Hyprland.get_default()
 
-  const workspaces = createBinding(hypr, "workspaces").as(workspaces => {
-    return [...workspaces].sort((a, b) => a.id - b.id)
+  const workspaces = createBinding(hypr, "workspaces").as(workspaces =>
+    [...workspaces].sort((a, b) => a.id - b.id)
+  )
 
-  })
   const focusedWorkspace = createBinding(hypr, "focusedWorkspace")
 
   return (
@@ -16,21 +21,39 @@ export default function Workspaces() {
       <For each={workspaces}>
         {(workspace) => {
           const active = focusedWorkspace.as(
-            (focused) => focused?.id === workspace.id
+            focused => focused?.id === workspace.id
           )
+
+          const clients = createBinding(workspace, "clients")
+
+          const icon = clients.as(clients => {
+            const client = clients[0]
+            const className = client?.class
+            return getIcon(className)
+          })
 
           return (
             <button
               valign={Gtk.Align.CENTER}
               halign={Gtk.Align.CENTER}
               class={active.as(
-                (active) =>
+                active =>
                   `workspace ${active ? "workspace-active" : ""}`
               )}
-            />
+            >
+              {icon ? (
+                <image
+                  file={icon}
+                  pixelSize={20}
+                />
+              ) : (
+                <label label="X" />
+              )}
+            </button>
           )
         }}
       </For>
     </box>
   )
 }
+
---
+++ dotfiles/ags/.config/ags/widget/consts.ts
@@ -0,0 +1,7 @@
+const PATH_ASSETS = './assets'
+
+export const iconMap = {
+  "brave-browser": `${PATH_ASSETS}/brave-browser.svg`,
+  kitty: `${PATH_ASSETS}/terminal.svg`,
+  default: `${PATH_ASSETS}/default.svg`
+}
